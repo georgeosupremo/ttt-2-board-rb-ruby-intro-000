8d23aa86350bd8622cb5da1a7b5d1db106238f63
george.burton@wework.com <george.burton@wework.com>
2018-02-07 01:09:33 +0000

    automatically backed up by learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,5 +1,14 @@
-puts "   |   |   "
-puts "-----------"
-puts "   |   |   "
-puts "-----------"
-puts "   |   |   "
+row = [" ", " ", " "]
+
+# Position 1: Left
+row [0]
+
+# Position 2: Middle
+row [1]
+
+# Position 3: Middle
+row [2]
+
+#Move X to the Position 2, Middle
+row[1] = "X"
+row #=> ["   ","X", "   "]
